688f6fb58f395d74db1b19db2230c03b620fd437
Jake Woods <jake@jakewoods.net>
2019-12-28 20:27:26 +1100

    Add patrolling ghosts

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour {
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
+    int currentWaypointIndex = 0;
+
+    void Start() {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    void Update() {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance) {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+
+            var currentWaypoint = waypoints[currentWaypointIndex];
+            navMeshAgent.SetDestination(currentWaypoint.position);
+        }
+    }
+}
