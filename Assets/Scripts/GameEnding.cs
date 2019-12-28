deddd49842d131d02ddd8088eeb269b2fa59c73d
Jake Woods <jake@jakewoods.net>
2019-12-28 21:55:54 +1100

    Parameterize GameEnding

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,56 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* Triggers the end of game cutscene on demand, used for victory and failure.
+ */
 public class GameEnding : MonoBehaviour {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
-    public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup;
-    public CanvasGroup caughtBackgroundImageCanvasGroup;
-    public AudioSource exitAudio;
-    public AudioSource caughtAudio;
 
-    bool hasWonGame = false;
-    bool hasLostGame = false;
-    float fadeTimer = 0f;
+    [Serializable]
+    public enum GameEndOptions { Restart, Quit }
+    public GameEndOptions gameEndOption = GameEndOptions.Restart;
+
+    public CanvasGroup endBackgroundImageCanvasGroup;
+    public AudioSource endAudio;
+
+    bool endTriggered = false;
     bool hasAudioPlayed = false;
-
-    public void CaughtPlayer() {
-        hasLostGame = true;
-    }
+    float fadeTimer = 0f;
 
     void Update() {
-        if(hasWonGame) {
-            EndLevel(exitBackgroundImageCanvasGroup, exitAudio, false);
-        } else if(hasLostGame) {
-            EndLevel(caughtBackgroundImageCanvasGroup, caughtAudio, true);
-        }
-    }
+        if(endTriggered) {
+            fadeTimer += Time.deltaTime;
+            endBackgroundImageCanvasGroup.alpha = fadeTimer / fadeDuration;
 
-    void OnTriggerEnter(Collider other) {
-        if(other.gameObject == player) {
-            hasWonGame = true;
-        }
-    }
+            if(!hasAudioPlayed) {
+                endAudio.Play();
+                hasAudioPlayed = true;
+            }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, AudioSource audioSource, bool doRestart) {
-        fadeTimer += Time.deltaTime;
-        imageCanvasGroup.alpha = fadeTimer / fadeDuration;
-
-        if(!hasAudioPlayed) {
-            audioSource.Play();
-            hasAudioPlayed = true;
-        }
-
-        // Quit the game when we're done fading
-        if(fadeTimer > fadeDuration + displayImageDuration) {
-            if(doRestart) {
-                SceneManager.LoadScene(0);
-            } else {
-                Application.Quit();
+            if(fadeTimer > fadeDuration + displayImageDuration) {
+                if(gameEndOption == GameEndOptions.Restart) {
+                    SceneManager.LoadScene(0);
+                } else if (gameEndOption == GameEndOptions.Quit) {
+                    Application.Quit();
+                }
             }
         }
+    }
+
+    public void EndGame() {
+        endTriggered = true;
     }
 }
--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour {
     public Transform player;
-    public GameEnding gameEnding;
+    public bool checkLineOfSight = false;
 
+    GameEnding gameEnding;
     bool isPlayerInRange = false;
+
+    void Start() {
+        gameEnding = GetComponent<GameEnding>();
+    }
 
     void OnTriggerEnter(Collider other) {
         if(other.transform == player) {
@@ -21,14 +26,22 @@ public class Observer : MonoBehaviour {
     }
 
     void Update() {
-        if(isPlayerInRange) {
+        bool playerInSight = isPlayerInRange;
+
+        if(isPlayerInRange && checkLineOfSight) {
             Vector3 direction = player.position - transform.position + Vector3.up;
 
             Ray ray = new Ray(transform.position, direction);
             RaycastHit raycastHit;
             if(Physics.Raycast(ray, out raycastHit) && raycastHit.collider.transform == player) {
-                gameEnding.CaughtPlayer();
+                playerInSight = true;
+            } else {
+                playerInSight = false;
             }
+        }
+
+        if(playerInSight) {
+            gameEnding.EndGame();
         }
     }
 }
